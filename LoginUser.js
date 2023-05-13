ffbb1990bc01bb926f4a601fbea2fb5ff668465e
Abhilash6766 <123532620+Abhilash6766@users.noreply.github.com>
2023-05-13 11:57:47 +0530

    Update LoginUser.js

--- LoginUser.js
+++ LoginUser.js
@@ -1,22 +1,19 @@
 import React,{useState} from "react"
-// import { useHistory } from 'react-router-dom';
-// import {Component} from "react"
-// import React from "react";
-// import Nav from "react-bootstrap/Nav";
-// import Navbar from "react-bootstrap/Navbar";
-import {Button} from "react-bootstrap"
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import {Container ,Button} from "react-bootstrap"
 import { Link } from "react-router-dom"
 import "./Login.css"
+import UserService from "../services/UserService";
+import Election from "./Election";
 function Login(){
-    // const history = useHistory();
 
     const [uname,setUname]=useState("")
     const [pass,setPass]=useState("")
     const[aid,setAid]=useState("")
+    const[user,setUser]=useState('')
+    const[login,setLogin]=useState(false)
 
-    // const handleButtonClick = () => {
-    //     history.push('welcome.js');
-    //   };
     const handleAidChange=(event)=>{
         setAid(event.target.value);
     };
@@ -27,35 +24,53 @@ function Login(){
       const handlePasswordChange = (event) => {
         setPass(event.target.value);
       };
-      const final=(event)=>
+      const handleLogin=(event)=>
       {
         event.preventDefault();
+        if(uname==='' || aid==='' || pass==='')
+        alert("enter complete deatils")
+        else{
+        UserService.getUserById(aid).then((res)=>{
+          setUser(res.data)
+          console.log(res.data)
+          setLogin(true)
+        })
       }
-      
+      }
 
+      if(login){
+        if(uname===user.username && pass===user.password){
+          return <Election/>
+        }
+        else{
+          setLogin(false)
+          alert("Invalid username or password")
+        }
+      }
 
       return (
         <div>
-            {/* <Navbar bg="danger" expand="lg" variant="dark">
-      <Container>
-        <h1 className="text">Online Voting </h1>
+            <Navbar bg="secondary" expand="lg" variant="dark">
+      <Container >
+        <h1 className="tt">Online Voting </h1>
           <Nav className="me-auto">
-            <Button variant="light"className="btn" >User</Button>
-            <Button variant="light" className="btn">Register</Button>
-            <Button variant="light" className="btn">Admin</Button>
+            <Button variant="light"className="btn" ><Link to='/'>Home</Link></Button>
+            <Button variant="light"className="btn" ><Link to='/user'>User</Link></Button>
+            <Button variant="light" className="btn"><Link to='/register'>Register</Link></Button>
+            <Button variant="light" className="btn"><Link to='/admin'>Admin</Link></Button>
           </Nav>
       </Container>
-    </Navbar> */}
+    </Navbar>
     <h3 className="heading">Please Login</h3>
     <div className="login">
-            <form onSubmit={final}>
+            <form>
             <label>
           Username:
           <input type="text" value={uname} onChange={handleUsernameChange} />
         </label>
         <br />
         <label>
-        Adhar ID:
+        College ID:
           <input type="number" value={aid} onChange={handleAidChange} />
         </label>
         <br />
@@ -64,7 +79,7 @@ function Login(){
           <input type="password" value={pass} onChange={handlePasswordChange} />
         </label>
         <br />
-        <Button variant="secondary" type="submit  ">Login</Button>
+        <Button variant="secondary" onClick={handleLogin} >Login</Button>
             </form>
             Do not have an account ?
             <Button variant="secondary"><Link to='/register' className="regbtn">Register</Link></Button>
